bd1154a571024a700acb1e98385928196cb20a45
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-06 21:55:30 +0300

    hata çözüldü

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment.development';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+ firebaseConfig:{
+    apiKey: "AIzaSyDTzaI2hdRqKGipeylmMwIDssLT5At9l4o",
+    authDomain: "alican-4608f.firebaseapp.com",
+    projectId: "alican-4608f",
+    storageBucket: "alican-4608f.appspot.com",
+    messagingSenderId: "240620281069",
+    appId: "1:240620281069:web:88b1f0b72385f7f00300e4",
+    measurementId: "G-ZJS5Z8GS0P"
+  }
+};
